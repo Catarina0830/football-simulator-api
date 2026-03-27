85418441f164a90fa268569ebcb8c09dbfad81b5
catarina_klein <catarina_klein@estudante.sesisenai.org.br>
2026-03-27 15:53:07 -0300

    feat: cria PlayerRepository e move lógica de persistência do banco

--- src/main/java/com/catarinaklein/footballsimulator/FootballSimulatorApiApplication.java
+++ src/main/java/com/catarinaklein/footballsimulator/FootballSimulatorApiApplication.java
@@ -2,14 +2,10 @@ package com.catarinaklein.footballsimulator;
 
 
 import com.catarinaklein.footballsimulator.model.Player;
+import com.catarinaklein.footballsimulator.repository.PlayerRepository;
 import com.catarinaklein.footballsimulator.service.PlayerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 
 
 @SpringBootApplication
@@ -23,37 +19,9 @@ public class FootballSimulatorApiApplication {
 
 		Player player = new Player("Chigiri", 80, 60, 90, 100);
 
-		// 🔴 DIP QUEBRADO: conexão direta com banco (alto acoplamento)
-		try {
-			Connection conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/test",
-					"root",
-					"1234"
-			);
-
-
-			// 🔴 SRP + OCP QUEBRADOS: SQL direto dentro da lógica
-			PreparedStatement stmt = conn.prepareStatement(
-					"INSERT INTO player (name, shoot, defense, speed, stamina) VALUES (?, ?, ?, ?, ?)"
-			);
-
-
-			stmt.setString(1, player.getName());
-			stmt.setInt(2, player.getShoot());
-			stmt.setInt(3, player.getDefense());
-			stmt.setInt(4, player.getSpeed());
-			stmt.setInt(5, player.getStamina());
-
-
-			stmt.executeUpdate();
-
-
-			System.out.println("Jogador salvo no banco!");
-
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		// 🔴 conexao com banco
+		PlayerRepository repository = new PlayerRepository();
+		repository.salvarPlayer(player);
 
 
 		// 🔴 lógica do service
