c200835ff87e88bab21f7972775682f2a46de784
catarina_klein <catarina_klein@estudante.sesisenai.org.br>
2026-04-02 21:14:14 -0300

    feat: adiciona PlayerResponseDTO record

---
+++ src/main/java/com/catarinaklein/footballsimulator/responseDTO/PlayerResponseDTO.java
@@ -0,0 +1,9 @@
+package com.catarinaklein.footballsimulator.responseDTO;
+
+public record PlayerResponseDTO(int id,
+                                String name,
+                                int shoot,
+                                int defense,
+                                int speed,
+                                int stamina
+){}
